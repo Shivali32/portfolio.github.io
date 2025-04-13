5fbe5459e5731eaebfcec708b51d259b840414e6
shimate <shimate@iu.edu>
2025-04-13 19:59:01 -0400

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,8 @@ const Navigation = React.forwardRef((props, ref) => {
     //     }`}
       expand="lg"
     >
-      <Navbar.Brand className="navbar-brand" href={process.env.PUBLIC_URL + "/home/#home"}>
+      {/* <Navbar.Brand className="navbar-brand" href={process.env.PUBLIC_URL + "/#home"}> */}
+      <Navbar.Brand className="navbar-brand" href="#home">
         {`<${mainBody.firstName} />`}
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
@@ -54,7 +55,8 @@ const Navigation = React.forwardRef((props, ref) => {
           {about.show && (
             <NavLink
               className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/home/#aboutme"}
+              //   href={process.env.PUBLIC_URL + "/#aboutme"}
+              href="#aboutme"
             >
               About
             </NavLink>
@@ -62,7 +64,8 @@ const Navigation = React.forwardRef((props, ref) => {
           {experiences.show && (
             <NavLink
               className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/home/#experiences"}
+              //   href={process.env.PUBLIC_URL + "/#experiences"}
+              href="#experiences"
             >
               Experiences
             </NavLink>
@@ -70,7 +73,8 @@ const Navigation = React.forwardRef((props, ref) => {
           {repos.show && (
 
             <NavLink
-              href={process.env.PUBLIC_URL + "/home/#projects"}
+            //   href={process.env.PUBLIC_URL + "/#projects"}
+              href="#projects"
             >
               Projects
             </NavLink>
@@ -86,7 +90,8 @@ const Navigation = React.forwardRef((props, ref) => {
           {skills.show && (
             <NavLink
               className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/home/#skills"}
+              //   href={process.env.PUBLIC_URL + "/#skills"}
+              href="#skills"
             >
               Skills
             </NavLink>
@@ -94,7 +99,8 @@ const Navigation = React.forwardRef((props, ref) => {
           {getInTouch.show && (
             <NavLink
               className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/home/#contactme"}
+              //   href={process.env.PUBLIC_URL + "/#contactme"}
+              href="#contactme"
             >
               Contact Me
             </NavLink>
